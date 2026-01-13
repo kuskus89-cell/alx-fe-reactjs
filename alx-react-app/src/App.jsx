7c2243cb9393e82a13a7e5d8db0959ebe705f8e2
Sundus <sundusnoor89@gmail.com>
2026-01-13 17:36:22 +0300

    added a jsx component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,8 +8,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <WelcomeMessage>
       <div>
+        <WelcomeMessage />
+        <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-  </WelcomeMessage>
+  </div>
   )
 }
 
