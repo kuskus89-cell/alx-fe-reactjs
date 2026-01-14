0f6d70740c1ec13a5febb6f1c5b42a182ce78da7
Sundus <sundusnoor89@gmail.com>
2026-01-14 15:40:07 +0300

    adding components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,9 @@
-import WelcomeMessage from './components/WelcomeMessage'
+import WelcomeMessage from './WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from '/Header'
+import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
 
