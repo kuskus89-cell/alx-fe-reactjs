b66489c3a8ec3cc57686a6f25238562e9e19dee9
Sundus <sundusnoor89@gmail.com>
2026-01-14 15:29:12 +0300

    Trying components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,9 +3,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from '/Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from '/Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -14,7 +15,7 @@ function App() {
         <Header/>
         <MainContent/>
         <Footer/>
-        <WelcomeMessage />
+        <WelcomeMessage/>
         <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -22,8 +23,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        
-        
+
       </div>
       <h1>Vite + React</h1>
       <div className="card">
