41a0b39a9531df732d5fb37b90c7c6656dabb806
Sundus <sundusnoor89@gmail.com>
2026-01-18 09:50:54 +0300

    Inline css

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return(
+    <main>
+         <p style={{ color: 'purple', padding:'20px'}}> I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+    );
+}
+
+export default MainContent;
