8115520e0daf3cdb4b0d1308879d13ef987a88f7
Sundus <sundusnoor89@gmail.com>
2026-01-18 22:10:09 +0300

    React routing

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
