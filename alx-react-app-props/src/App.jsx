a945d33806a7cbf506e352e2f579776823e4dd29
Sundus <sundusnoor89@gmail.com>
2026-01-18 16:37:32 +0300

    Context drilling

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import UserContext from './context/UserContext.js'
+import UserContext from './UserContext.js'
 
 function App() {
   const [count, setCount] = useState(0)
