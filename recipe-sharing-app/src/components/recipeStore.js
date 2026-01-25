57dc9b45c0ca7b607dc2550be889370ee725f0c7
Sundus <sundusnoor89@gmail.com>
2026-01-25 20:58:57 +0300

    Advanced zustand

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,21 +1,25 @@
-import create from 'zustand'
+import create from 'zustand';
+import React from 'react';
+import { useRecipeStore } from './recipeStore';
 
 const useRecipeStore = create(set => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
+  filterRecipes: () => set(state => ({filteredRecipes: state.recipes.filter(recipe =>
+recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+  ))
+  })),
 
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+  const SearchBar = () => {
+  const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
 
-  updateRecipe: (updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
-    })),
-}));
-
-
+  return (
+    <input
+      type="text"
+      placeholder="Search recipes..."
+      onChange={(e) => setSearchTerm(e.target.value)}
+    />
+  );
+};
