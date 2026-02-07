5f7f38aa88738fc5df8920de33ea4bd577432255
Sundus Noor <sundusnoor89@gmail.com>
2026-02-07 21:20:38 +0300

    react and tailwind css

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,9 +3,9 @@
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg user-profile">
-      <img className="rounded-full mx-auto w-36 h-36" src="https://via.placeholder.com/150" alt="User" />
-      <h1 className="text-x1 text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <img className="rounded-full mx-auto w-sm-24 h-sm-24 w-36 h-36" src="https://via.placeholder.com/150" alt="User" />
+      <h1 className="text-xl text-lg-sm text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 text-sm text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
