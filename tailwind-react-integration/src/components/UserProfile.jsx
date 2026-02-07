f92e8c09bde4c40f5a09bf37197e30bf7c42ae37
Sundus Noor <sundusnoor89@gmail.com>
2026-02-07 21:47:54 +0300

    adding advanced tailwind css on the userprofile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,9 +2,9 @@
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 md:p-8 sm:p-4 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg"user-profile>
-      <img className="rounded-full mx-auto sm:w-24 h-sm:h-24 md:w-36 md:h-36" src="https://via.placeholder.com/150" alt="User" />
-      <h1 className="md:text-xl text-lg-sm text-blue-800 my-4">John Doe</h1>
+    <div className=" hover:shadow-xl bg-gray-100 md:p-8 sm:p-4 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg"user-profile>
+      <img className="hover:scale-110 transition-transform duration-300 ease-in-out rounded-full mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36" src="https://via.placeholder.com/150" alt="User" />
+      <h1 className="hover:text-blue-500 md:text-xl text-lg-sm text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-sm text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
