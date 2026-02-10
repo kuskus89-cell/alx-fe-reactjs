871ba0eaf073cd53c638797f2f317a802ba001a7
Sundus Noor <sundusnoor89@gmail.com>
2026-02-10 12:07:51 +0300

    adding tailwind css to react app

---
+++ recipe-sharing-platform/tailwindconfig.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
