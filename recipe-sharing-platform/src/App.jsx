e41e1ece11e7e531098ff81003a1cbd78be0cd87
Sundus Noor <sundusnoor89@gmail.com>
2026-02-10 12:51:14 +0300

    styling using tailwind css

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import User from './components/user'
-import HomePage from './components/homepage'
+import HomePage from './components/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
