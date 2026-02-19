3606dcd67680fb23665c8fac69ba6a098745c1bf
Sundus Noor <sundusnoor89@gmail.com>
2026-02-19 11:33:55 +0300

    advanced react router dom

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,13 +1,13 @@
 import { Navigate } from "react-router-dom";
 
-function ProtectedRoute({ children }) {
-  const isAuthenticated = false; // simulate login
 
-  if (!isAuthenticated) {
+  const auth = { isAuthenticated: true }; // simulate login
+
+  function ProtectedRoute({ children }) {
+  if (!auth.isAuthenticated) {
     return <Navigate to="/" replace />;
   }
 
   return children;
-}
-
+} 
 export default ProtectedRoute;
