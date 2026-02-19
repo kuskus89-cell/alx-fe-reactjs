01fd583ff3908215289ca5c60838b1fba4abbe2c
Sundus Noor <sundusnoor89@gmail.com>
2026-02-19 11:26:56 +0300

    advanced react router dom

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,7 @@
-import { Link, Outlet } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import { ProfileDetails } from "./ProfileDetails";
+import { ProfileSettings } from "./ProfileSettings";
+
 
 function Profile() {
   return (
@@ -10,7 +13,12 @@ function Profile() {
         <Link to="settings">ProfileSettings</Link>
       </nav>
 
-      <Outlet />
+ <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+
+
     </div>
   );
 }
